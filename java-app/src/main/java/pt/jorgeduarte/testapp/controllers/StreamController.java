dcd31c34f6c0d3910b877859919805a379d44ea6
Joka duarte <Joka867@hotmail.com>
2024-05-27 17:38:42 +0100

    feat: added missing files

---
+++ java-app/src/main/java/pt/jorgeduarte/testapp/controllers/StreamController.java
@@ -0,0 +1,20 @@
+package pt.jorgeduarte.testapp.controllers;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+@RestController
+@RequestMapping("/stream")
+public class StreamController {
+    @GetMapping(value = "/data", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<String> streamData() {
+            return Flux.interval(Duration.ofSeconds(1))
+                    .take(5)
+                    .map( sequence -> "Stream" + sequence + " ")
+                    .onErrorResume(ex -> Flux.just("Something went wrong: " + ex.getMessage()));
+    }
+}
